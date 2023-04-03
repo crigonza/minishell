1b8b50edc87090245166ccbfdad21e331e477819
ipaelo <ipaelo@gmail.com>
2023-04-03 09:46:17 +0200

    Corregido ^C

--- src/signal.c
+++ src/signal.c
@@ -6,32 +6,28 @@
 /*   By: itorres- <itorres-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:57:49 by itorres-          #+#    #+#             */
-/*   Updated: 2023/03/30 14:57:51 by itorres-         ###   ########.fr       */
+/*   Updated: 2023/04/03 09:35:29 by itorres-         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../inc/minishell.h"
 
 void	process_signal(int signum)
-{
-	if (signum == SIGINT)
-    {
+{		
+		(void) signum;
+    
 		write(1, "\n", 1);
 		rl_on_new_line(); // LLama al prompt en una nueva linea
 		rl_replace_line("", 0);  //limpia el texto previo
 	    rl_redisplay(); // Cambia el contenido por el que tenga el buffer 
-    }
+		
+    
 }
 
 void	config_signals(void)
 {
-	struct sigaction	sa;
-
-	sa.sa_handler = &process_signal;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGINT);
-	sigaction(SIGINT, &sa, NULL);  /* Cambiamos la forma de trabajar de Control + C */
-	sigaction(SIGTERM, &sa, NULL);  /* Cambiamos la forma de trabajar de Control + D */
+	rl_catch_signals = 0;
+	signal(SIGINT, process_signal); /* Cambiamos la forma de trabajar de Control + C */
+	signal(SIGTERM, SIG_IGN); /* Cambiamos la forma de trabajar de Control + D */
 	signal(SIGQUIT, SIG_IGN);      /* Ignora Control + / */
 }
